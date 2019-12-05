4aa1f89d2e7031fb4accd5030c842cc630f9d14b
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-05 23:04:14 +0200

    Updates

---
+++ precache-manifest.74a4a314692576611cd601741f4cb1f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0364eb0194f9682ec2ee2a254bfe85ce",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "00c2a52596b69cba5ce6",
+    "url": "/react_tabs/static/css/main.79ccff04.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/react_tabs/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "00c2a52596b69cba5ce6",
+    "url": "/react_tabs/static/js/main.7810d512.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
